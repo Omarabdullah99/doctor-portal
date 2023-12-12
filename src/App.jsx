c6f91d046e6734301f38dd30320eb604504d8d6d
Omarabdullah99 <omarabdullah917303@gmail.com>
2023-12-12 15:32:58 +0600

    protected&privateroute

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
-import { useSelector } from 'react-redux'
-import Spinner from './components/Spinner'
-
-
+import { useSelector } from "react-redux";
+import Spinner from "./components/Spinner";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
 
 
 function App() {
   const { loading } = useSelector((state) => state.alerts);
   return (
     <>
-    <BrowserRouter>
-    {loading ? (<Spinner />) : (
-    <Routes>
-      <Route path='/' element={<HomePage  />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/register' element={<RegisterPage />} />
-    </Routes>
-    ) }
-    </BrowserRouter>
+      <BrowserRouter>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Routes>
+            <Route path="/" element={<ProtectedRoute><HomePage /></ProtectedRoute> } />
+
+            <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute> } />
+
+            <Route path="/register" element={<PublicRoute><RegisterPage /></PublicRoute> } />
+          </Routes>
+        )}
+      </BrowserRouter>
     </>
-  
-  )
+  );
 }
 
-export default App
+export default App;
