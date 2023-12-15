322f79f35d9647f274c8680c29f0dfa84ab3411e
Omarabdullah99 <omarabdullah917303@gmail.com>
2023-12-15 14:25:04 +0600

    aproveDoctor-admin&doctor-doctoruserId-shownoti

--- src/pages/admin/Doctors.jsx
+++ src/pages/admin/Doctors.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Table } from "antd";
+import { message,Table } from "antd";
 import { useEffect, useState } from "react";
 import Layout from "../../components/Layout"
 
@@ -25,6 +25,26 @@ const Doctors = () => {
     getDoctors();
   }, []);
 
+  const handleAccountStatus = async (record, status) => {
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/v1/admin/changeAccountStatus",
+        { doctorId: record._id, userId: record.userId, status: status },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (res.data.success) {
+        message.success(res.data.message);
+        window.location.reload();
+      }
+    } catch (error) {
+      message.error("Something Went Wrong");
+    }
+  }
+
   const columns = [
     {
       title: "Name",
@@ -49,7 +69,7 @@ const Doctors = () => {
       render: (text, record) => (
         <div className="d-flex">
           {record.status === "pending" ? (
-            <button className="btn btn-success">Approve</button>
+            <button className="btn btn-success"onClick={() => handleAccountStatus(record, "approved")}>Approve</button>
           ) : (
             <button className="btn btn-danger">Reject</button>
           )}
